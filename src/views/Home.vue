20feccc24175b67304b8b47252a1d924150f3434
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-01 10:28:54 -0500

    add mapbox to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div id='map' style='width: 400px; height: 300px;'></div>
   </div>
+
 </template>
 
+
+<style>
+
+</style>
+
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  mounted: function() {
+    this.doMapBox();
+  },
+  methods: {
+    doMapBox: function() {
+      mapboxgl.accessToken = process.env.VUE_APP_MAP_API_KEY;
+      const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+      });
+    }
   }
-}
+};
 </script>
